c481f3223c366fba6ee020fd29ad71d7b39b506c
JavanFriedel <JavanFriedel@hotmail.com>
2022-02-11 14:56:39 -0500

    Recoded to work with UI instead of a looped prompt

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Randomly assigns what the computer will play and returns the falue
 function computerPlay (){
     let choice = Math.floor(Math.random()*100) % 3;    
-    return choice == 0 ? 'Rock': choice == 1 ? 'Paper': 'Scissors'
+    return choice == 0 ? 'rock': choice == 1 ? 'paper': 'scissors'
 }
 
 //takes player selection and computer selection and returns who won or tie
-function playRound (playerSelection, computerSelection){
+function playRound (playerSelection){
     let playerSelect = playerSelection.toLowerCase()
-    let computerSelect = computerSelection.toLowerCase()
+    let computerSelect = computerPlay().toLowerCase()
 
 
     //this is not good code for readability. I just wanted to practice using ternary operators
@@ -20,32 +20,35 @@ function playRound (playerSelection, computerSelection){
     }
 }
 
-//plays a game and takes number of rounds as an argument
-function game (numRounds) {
-    let playerWins = 0;
-    let computerWins = 0;
+let bestOf = 3
+let computerWins = 0
+let playerWins = 0
 
-    //!needs to be refactored to play rounds untill best of is hit.
-    for (let i = 0; i < numRounds; i++){
-        let playerChoice = prompt('Rock, Paper or scissors');
+const rockBtn = document.getElementById('rock')
 
-        let outcome = playRound(playerChoice, computerPlay());
+rockBtn.addEventListener('click', () => {
+    //play a round against computer with selection
+    let outcome = playRound('rock')
+    //log the outcome
+    console.log(outcome)
 
-        if (outcome == 'Computer Wins'){
-            computerWins++;
-            console.log('Computer Wins. Current Wins' + computerWins)
-        } else if (outcome == 'Player Wins'){
-            playerWins++;
-            console.log('Player Wins. Current ' + playerWins)
-        } else {
-            numRounds++;
-            console.log('Tie Game!')
-        }
-        
+    //update standing score
+    if (outcome == 'Computer Wins'){
+        computerWins++
+    } else if (outcome == 'Player Wins'){
+        playerWins++
     }
 
-    return playerWins > computerWins ? 'Player Wins' : 'Player Wins'
-}
+    //check if game is over
+    if (computerWins >= bestOf){
+        console.log('Computer Wins the Game')
+    }else if (playerWins >= bestOf){
+        console.log('Player wins the game!')
+    }
+})
+
+// Event listeneers
+
 
 // ! TO DO
 // - make a function to log outcomes for each round
